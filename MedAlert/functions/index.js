f5856f294a34d50a841de0ec533898b1285a49c6
Jajared <jajawong09@gmail.com>
2023-07-06 14:47:06 +0800

    Fixed firebase function.

--- MedAlert/functions/index.js
+++ MedAlert/functions/index.js
@@ -7,56 +7,25 @@ admin.initializeApp();
 const firestore = admin.firestore();
 
 exports.resetScheduledData = onSchedule("00 16 * * *", async () => {
-  function getScheduledItems(allMedicationItems) {
+  function resetScheduledItems(scheduledItems) {
     var temp = [];
-    for (let i = 0; i < allMedicationItems.length; i++) {
-      const timeInterval = 24 / allMedicationItems[i].Instructions.FrequencyPerDay;
-      for (let j = 0; j < allMedicationItems[i].Instructions.FrequencyPerDay; j++) {
-        temp.push({
-          ...allMedicationItems[i],
-          Acknowledged: false,
-          Instructions: {
-            ...allMedicationItems[i].Instructions,
-            FirstDosageTiming: allMedicationItems[i].Instructions.FirstDosageTiming + timeInterval * 60 * j > 24 * 60 ? allMedicationItems[i].Instructions.FirstDosageTiming + timeInterval * 60 * j - 24 * 60 : allMedicationItems[i].Instructions.FirstDosageTiming + timeInterval * 60 * j,
-          },
-        });
-      }
+    for (let i = 0; i < scheduledItems.length; i++) {
+      temp.push({
+        ...scheduledItems[i],
+        Acknowledged: false,
+      });
     }
-    var result = sortScheduledItems(temp);
-    return result;
+    return temp;
   }
-  // Sort scheduled items in order of time
-  function sortScheduledItems(data) {
-    var scheduledItemsInOrder = [];
-    var lowestTime = 24 * 60;
-    var prevLowest = -1;
-    while (data.length != scheduledItemsInOrder.length) {
-      for (let i = 0; i < data.length; i++) {
-        if (data[i].Instructions.FirstDosageTiming < lowestTime && data[i].Instructions.FirstDosageTiming > prevLowest) {
-          lowestTime = data[i].Instructions.FirstDosageTiming;
-        }
 
-        if (i == data.length - 1) {
-          prevLowest = lowestTime;
-          for (let j = 0; j < data.length; j++) {
-            if (data[j].Instructions.FirstDosageTiming == lowestTime) {
-              scheduledItemsInOrder.push(data[j]);
-            }
-          }
-          lowestTime = 24 * 60;
-        }
-      }
-    }
-    return scheduledItemsInOrder;
-  }
   try {
     firestore
       .collection("MedicationInformation")
       .get()
       .then((snapshot) => {
         snapshot.forEach((doc) => {
-          const medicationItems = doc.data().MedicationItems;
-          doc.ref.update({ ScheduledItems: getScheduledItems(medicationItems) });
+          const scheduledItems = doc.data().ScheduledItems;
+          doc.ref.update({ ScheduledItems: resetScheduledItems(scheduledItems) });
         });
       });
     return null;
